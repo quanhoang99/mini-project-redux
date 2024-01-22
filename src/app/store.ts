2d6ff8b1ef2f38b21dcce35694a7287901eefc49
IPA\quan.nguyenhoang2 <quan.nguyenhoang2@ivnd.com.vn>
2024-01-22 17:44:23 +0700

    config project

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,17 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import createSagaMiddleware from 'redux-saga';
 
+let middlewares: any = [];
+const sagaMiddleware = createSagaMiddleware();
+middlewares = [...middlewares, sagaMiddleware];
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
 });
 
+sagaMiddleware.run(rootSaga);
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
